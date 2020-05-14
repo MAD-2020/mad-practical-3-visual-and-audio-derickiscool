c3a3af3015fcbc930c64dd3c870cd4f3e29d64c2
Derick <derickwithoner@gmail.com>
2020-05-14 21:37:57 +0800

    Finished practical

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,12 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.media.AudioAttributes;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer babyLaughing = MediaPlayer.create(this,R.raw.baby_laughing);
+        babyLaughing.start();
+        VideoView rabbidVideo=findViewById(R.id.rabbidVideo);
+        rabbidVideo.setVideoURI(Uri.parse("android.resource://" +getPackageName() + "/" + R.raw.rabbid));
+        rabbidVideo.start();
+
+
     }
 }
